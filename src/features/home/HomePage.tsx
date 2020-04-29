44be99e1a0d5e7fdee8f349b6cb2344ffbec7fd9
JamesDimitrako <dimdimitrako@gmail.com>
2020-04-29 19:40:21 +0300

    Styling home page

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Segment, Header, Button, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <Container style={{ marginTop: "7em" }}>
-      <h1>Home page</h1>
-      <h3>
-        Go to <Link to="/tickets">Tickets</Link>
-      </h3>
-    </Container>
+    <Segment inverted textAlign="center" vertical className="masthead">
+      <Container text>
+        <Header as="h1" inverted>
+          <Image
+            size="massive"
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginBottom: 12 }}
+          />
+          Sharp.Tickets
+        </Header>
+        <Header as="h2" inverted content="Welcome to Sharp.Tickets" />
+        <Button as={Link} to="/tickets" size="huge" inverted>
+          Take me to the tickets!
+        </Button>
+      </Container>
+    </Segment>
   );
 };
 export default HomePage;
