7a31ce00a16036e9b3ed6d2ce66b376717281c70
JamesDimitrako <dimdimitrako@gmail.com>
2020-06-08 22:37:07 +0300

    Client Side Login and Register interfaces and Axios Methods

--- src/app/api/agent.ts
+++ src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { ITicket } from "../models/ticket";
 import { history } from "../..";
 import { toast } from "react-toastify";
+import { IUser, IUserFormValues } from "../models/user";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
@@ -49,6 +50,14 @@ const Tickets = {
   update: (ticket: ITicket) => requests.put(`/tickets/${ticket.id}`, ticket),
   create: (ticket: ITicket) => requests.post(`/tickets`, ticket),
   delete: (id: string) => requests.delete(`/tickets/${id}`),
+};
+
+const User = {
+  current: (): Promise<IUser> => requests.get("/user"),
+  login: (user: IUserFormValues): Promise<IUser> =>
+    requests.post(`/user/login`, user),
+  register: (user: IUserFormValues): Promise<IUser> =>
+    requests.post(`/user/register`, user),
 };
 
 export default {
