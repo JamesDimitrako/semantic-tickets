31e059cd600821e904caf6a7a9b79a95cb145d3d
JamesDimitrako <dimdimitrako@gmail.com>
2020-06-09 00:01:02 +0300

    Login -> Using Axios request interceptor to automatically send the JWT Token

--- src/app/stores/rootStore.ts
+++ src/app/stores/rootStore.ts
@@ -2,16 +2,19 @@ import TicketStore from "./ticketStore";
 import UserStore from "./userStore";
 import { createContext } from "react";
 import { configure } from "mobx";
+import CommonStore from "./commonStore";
 
 configure({ enforceActions: "always" });
 
 export class RootStore {
   ticketStore: TicketStore;
   userStore: UserStore;
+  commonStore: CommonStore;
 
   constructor() {
     this.ticketStore = new TicketStore(this);
     this.userStore = new UserStore(this);
+    this.commonStore = new CommonStore(this);
   }
 }
 
