adf08590d6a11c6a32d418aca6341b92547d247e
JamesDimitrako <dimdimitrako@gmail.com>
2020-06-09 00:46:21 +0300

    Adding Modals to our application

--- src/features/user/LoginForm.tsx
+++ src/features/user/LoginForm.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Form as FinalForm, Field } from "react-final-form";
-import { Form, Button, Label } from "semantic-ui-react";
+import { Form, Button, Label, Header } from "semantic-ui-react";
 import TextInput from "../../app/common/form/TextInput";
 import { RootStoreContext } from "../../app/stores/rootStore";
 import { IUserFormValues } from "../../app/models/user";
@@ -33,6 +33,12 @@ const LoginForm = () => {
         dirtySinceLastSubmit,
       }) => (
         <Form onSubmit={handleSubmit}>
+          <Header
+            as="h2"
+            content="Login to Sharp.Tickets"
+            color="teal"
+            textAlign="center"
+          />
           <Field name="email" component={TextInput} placeholder="Email" />
           <Field
             name="password"
@@ -47,10 +53,11 @@ const LoginForm = () => {
           <Button
             disabled={(invalid && !dirtySinceLastSubmit) || pristine}
             loading={submitting}
-            positive
+            color="teal"
             content="Login"
+            fluid
           />
-          <pre>{JSON.stringify(form.getState(), null, 2)}</pre>
+          {/* <pre>{JSON.stringify(form.getState(), null, 2)}</pre> */}
         </Form>
       )}
     />
